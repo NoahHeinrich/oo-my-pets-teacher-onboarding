c3793e02900926a552625a7038552074181e6ded
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-22 14:52:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -25,26 +25,26 @@ class Owner
   end
   
   def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+    self.pets[:fishes] << Fish.new(name)
   end
   
   def buy_cat(name)
-    @pets[:cats] << Cat.new(name)
+    self.pets[:cats] << Cat.new(name)
   end
   
   def buy_dog(name)
-    @pets[:dogs] << Dog.new(name)
+    self.pets[:dogs] << Dog.new(name)
   end
   
   def walk_dogs
-    @pets[:dogs].each { |dog| dog.mood = "happy" }
+    self.pets[:dogs].each { |dog| dog.mood = "happy" }
   end
   
   def play_with_cats
-    @pets[:cats].each { |cat| cat.mood = "happy" }
+    self.pets[:cats].each { |cat| cat.mood = "happy" }
   end
   
   def feed_fish
-    @pets[:fishes].each { |fish| fish.mood = "happy" }
+    self.pets[:fishes].each { |fish| fish.mood = "happy" }
   end
 end
